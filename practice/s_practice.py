eb474f0121668f40e3b2b9ce646b1a5b6b18395a
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-04-17 23:23:08 +0900

    schedule practice

--- practice/s_practice.py
+++ practice/s_practice.py
@@ -4,8 +4,13 @@ import time
 def message():
     print("스케쥴 실행중...")
 
-schedule.every(1).seconds.do(message)
+def message2(text):
+    print(text)
+
+#schedule.every(1).seconds.do(message)
+
+schedule.every(2).seconds.do(message2, '2초마다 알려줄게요')
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    #time.sleep(1)
